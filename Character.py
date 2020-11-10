fff6c594ea8c6b7ae4d9d617600bf3d0bce034af
Wade Cheng <2014wcheng@gmail.com>
2020-11-09 17:54:45 -0800

    added attributes to Character class to enable status effects

---
+++ Character.py
@@ -0,0 +1,44 @@
+class Character:
+    def __init__(
+        self,
+        name,
+        health,
+        is_stunned=False,
+        is_burning=False,
+        is_poisoned=False,
+        is_withered=False,
+    ):
+        self.name = name
+        self.health = health
+        self.is_stunned = is_stunned
+        self.is_burning = is_burning
+        self.is_poisoned = is_poisoned
+        self.is_withered = is_withered
+        # TODO: status effects
+
+    def takedamage(self, damage):
+        self.health -= damage
+
+    def printcharsheet(self):  # TODO: used for debugging, make prettier later
+        print(vars(self))
+
+
+class Player(Character):
+    def __init__(
+        self, name, health, is_stunned, is_burning, is_poisoned, is_withered, inventory
+    ):
+        super().__init__(name, health, is_stunned, is_burning, is_poisoned, is_withered)
+        self.inventory = inventory
+
+
+class Enemy(Character):
+    pass
+    # TODO: add favorite attack attirbute? add attack order attribute?
+    #  or should those be handled within the individual enemy classes?
+    #  example: Skeleton attacks with x, then y, then z, but will do f, then z if player below 30%.
+    #  will also do heal if an ally is below 10%
+
+
+class Skeleton(Enemy):
+    def bonesattack(self, target):
+        target.health -= 2
